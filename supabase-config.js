0455f03503d3c7ef5c7701cd6963f4bfda0e9967
mouazaymanfarghaly-create <mouazaymanfarghaly@gmail.com>
2026-08-21 15:20:45 +0300

    Create supabase-config.js

---
+++ supabase-config.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL = "https://meftmrgkqvdvurvrkrkc.supabase.co";
+const SUPABASE_KEY = "sb_publishable_IFLpHM_wfabV4dYtQXRTiA_CjFm-a5j";
+
+const supabaseClient = supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
+);
